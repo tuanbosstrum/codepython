0832d9519c2f6ccc094d4021515c242f67bddf08
tuan <tuanbosstrum@gmail.com>
2021-09-18 14:03:12 +0700

    ex3 l1

---
+++ bai4ex03.py
@@ -0,0 +1,10 @@
+tinh = ("Huế","Quảng Bình","Quảng Trị")
+mien_nam = ("TP.HCM","Đồng Nai","Bình Dương")
+
+print(tinh)
+print(mien_nam)
+
+ds_vung_do = tinh + mien_nam
+print(ds_vung_do)
+
+print(ds_vung_do[0])
