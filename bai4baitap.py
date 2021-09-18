92fe7fdc6fc82c9a29d07b979683aa9b76007491
tuan <tuanbosstrum@gmail.com>
2021-09-18 13:49:21 +0700

    up ex02

---
+++ bai4baitap.py
@@ -0,0 +1,6 @@
+thanh_vien = ["Khánh","Duy","An","Thơ","Huy"]
+print(thanh_vien)
+
+#SỬA THÔNG TIN
+thanh_vien[1] = "Tuấn"
+print(thanh_vien)
---
+++ bai4ex02.py
@@ -0,0 +1,21 @@
+danhsach = []
+hoten = input("Nhập họ tên:")
+danhsach.append(hoten)
+
+hoten = input("Nhập họ tên:")
+danhsach.append(hoten)
+
+hoten = input("Nhập họ tên:")
+danhsach.append(hoten)
+
+hoten = input("Nhập họ tên:")
+danhsach.append(hoten)
+
+hoten = input("Nhập họ tên:")
+danhsach.append(hoten)
+print(danhsach)
+
+vitri = int(input("Bạn muốn chỉnh vị trị thứ mấy:"))
+hoten = input("Nhập họ tên mới:")
+danhsach[vitri] = hoten
+print(danhsach)
