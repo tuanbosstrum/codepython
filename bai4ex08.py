301b4c3d4dde74a5b51caab9644130304ad2f975
tuan <tuanbosstrum@gmail.com>
2021-09-23 13:54:38 +0700

    up tiep lan 2

--- bai4ex08.py
+++ bai4ex08.py
@@ -1,7 +1,9 @@
-tong_tien = 0
+tong_tien = int(input("Bạn đã có bao nhiêu tiền:"))
 so_nguoi_dong_gop = 0
 while tong_tien <= 100000:
     dong_gop = int(input("Mời bạn góp tiền uống cà phê:"))
     tong_tien += dong_gop
     so_nguoi_dong_gop +=1
+else:
+    print("Đã có đủ tiền mời mọi người uống cà phê, quyên gì nữa!")
 print("Đã quyên góp được{} từ {} người!".format(tong_tien, so_nguoi_dong_gop))
---
+++ bai4viduwhile.py
@@ -0,0 +1,9 @@
+def main():
+    x=0
+    #define a while loop
+    while(x <4):
+        print(x)
+        x = x+1
+
+if __name__ == "__main__":
+   main()
