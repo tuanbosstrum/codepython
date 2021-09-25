04c2c118ad88654300e86be1504a2e8a6faac7fb
tuan <tuanbosstrum@gmail.com>
2021-09-25 13:31:32 +0700

    test loi goi ham

---
+++ loigoiham.py
@@ -0,0 +1,14 @@
+# def hello():
+#     print("Ciao Tuấn")
+# hello()
+
+# def pt(x):
+#     print(x*2)
+# pt(2)
+
+def tong():
+    a = 20
+    b = 200
+    s = a + b
+    print("tổng:" + str(s))
+tong()
